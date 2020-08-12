3d2263e1de924742935da51fb7279e96db0c2c8d
Wojtek Witkowski <hi@wojtek.im>
2020-08-12 16:35:41 +0200

    hi i know this is bad but it works

---
+++ index.js
@@ -0,0 +1,38 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const axios = require("axios");
+
+async function getCache() {
+  try {
+    const cache = await fs.promises.readFile("last_grab_url.txt", "utf8");
+    return cache;
+  } catch (fileError) {
+    console.log(fileError);
+    console.log('Run "touch last_grab_url.txt" to create the file first.');
+  }
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://screenhole.net");
+
+  const grab = await page.waitForSelector(".grabs-feed .grab:first-child .grab-upload a");
+  const query = await grab.evaluate((element) => element.href);
+
+  getCache().then((data) => {
+    const cachedGrab = data;
+
+    fs.writeFileSync("last_grab_url.txt", query);
+
+    if (query === cachedGrab) {
+      // nothing
+    } else {
+      axios.post(
+        `https://api.telegram.org/${process.env.TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${query}`
+      );
+    }
+  });
+
+  await browser.close();
+})();
